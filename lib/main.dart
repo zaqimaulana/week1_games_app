d73508651e96e95ad4e1914374e18d7dbd13ec12
zaqimaulana <zaqimaulana72@gmail.com>
2026-02-13 20:57:50 +0700

    mengubah stl jadi stfl dasar

--- lib/main.dart
+++ lib/main.dart
@@ -4,29 +4,6 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fruit Catcher Game',
-      home: const GameScreen(),
-    );
-  }
-}
-
-class GameScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Game akan ditampilkan di sini'),
-      ),
-    );
-  }
-}
-
 class GameScreen extends StatefulWidget {
   const GameScreen({super.key});
 
@@ -44,16 +21,42 @@ class _GameScreenState extends State<GameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Dashboard"),
-        actions: const [],
-      ),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-            children: [],
+      body: Stack(
+        children: [
+          Positioned(
+            top: 50,
+            left: 20,
+            child: Container(
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black54,
+                borderRadius: BorderRadius.circular(10),
+          ), 
+              child: const Text(
+                  'Score: 1',
+                    style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),//Container 
+            ), // Positioned
+          ],
         ),
+      );
+    }
+}
+
+class GameScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Game akan ditampilkan di sini'),
       ),
     );
   }
 }
+
+
